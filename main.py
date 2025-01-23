69686a993e0758d3c071a93078776007858a200d
MEHWISH <86172399+mehwishkk@users.noreply.github.com>
2025-01-23 22:41:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from tkinter import *
 import math
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
+
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
